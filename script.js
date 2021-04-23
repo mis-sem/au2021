5073afd2bd3959a4f7b78b2f2f32aeab2f95c135
clbokea <clbo@kea.dk>
2021-04-23 22:36:09 +0200

    licenced

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $().ready(function() {
 	$(".page-header a").hide()
 	$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
 	//$(".page-header").append("<div>Teacher: Claus Bove, clbo@kea.dk</div>")
-	$(".main-content").prepend("<div><i><small><a href='index.html'>Undervisningsplan</a> </small></i></div>")
+	$(".main-content").prepend("<div><i><small><a href='index.html'>Home</a> </small></i></div>")
 
-	$(".site-footer").append("<div style='text-align:right'>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <a href='changelog.md'>Licence change log</a></div>")
+	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a></small></div>")
 	$(".site-footer-owner").hide()
 
 	$(".main-content").attr('style', 'max-width:60rem; font-size:1.2rem');
