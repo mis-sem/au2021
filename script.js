a0728a0eba6862135a208722c7dd0fe1c6bc1fcc
clbokea <clbo@kea.dk>
2021-04-25 20:39:56 +0200

    script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $().ready(function() {
 	// $("h1:first").css("display", "none");
 	$("h1:first").html("./missing semester");
-	$(".page-header a").hide()
+	$(".page-header a:first").hide()
 	$(".page-header").append("<div>Elective at KEA CS Fall 2021</div>")
-	$(".page-header").append("<div style='text-align:right'>./missing_semester | lectures | about</div>")
-	$(".page-content").append("<div><i><small><a href='index.html'>Home</a> </small></i></div>")
+	$(".page-header").append("<div style='text-align:right'><a href=''>./missing_semester</a> | <a href=''>lectures</a> | <a href=''>about</a></div>")
+	//$(".page-content").append("<div><i><small><a href='index.html'>Home</a> </small></i></div>")
 
 	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br><a href='changelog.md'>Licence change log</a> <br> Claus Bove (clbo@kea.dk)</small></div>")
 	$(".site-footer-owner").hide()
         $(".site-footer-credits").hide()	
 	$(".main-content").attr('style', 'max-width:60rem; font-size:1.2rem');
-	$(".page-header").attr('style', 'paddin: 5rem 26rem');
+	$(".page-header").attr('style', 'padding: 5rem 26rem');
 
 });
 
